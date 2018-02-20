3ab481c7a46fd767714a8be38b0927d32c9700f9
haiyunx <haiyunxu@hotmail.com>
2018-02-20 19:19:48 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <a>Wine &amp; Spirits Magazine</a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">About</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default');
+@section('title','signup')
+
+
+@section('content')
+<h1>Sign up </h1>
+@stop

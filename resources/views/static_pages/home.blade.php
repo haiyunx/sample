db24a549f31b03042e4729e9d7899009e110e0c6
haiyunx <haiyunxu@hotmail.com>
2018-05-01 20:44:42 +0000

    Add user statuses

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,13 +1,30 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-        This is Helena's Test Website
-    </p>
-    <p>
-        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Sign Up</a>
-    </p>
-</div>
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h3>Post Lists</h3>
+        @include('shared._feed')
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+  <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+          This is Helena's Test Website
+      </p>
+      <p>
+          <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Sign Up</a>
+      </p>
+  </div>
+  @endif
 @stop
